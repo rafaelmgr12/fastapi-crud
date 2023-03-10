57bd3387c7711b5ad3efc23e8f03b35dd280e5b2
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-03-10 14:07:45 -0300

    [feat] add main

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
-from fastapi import FastAPI, APIRouter, status
+from src import model
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src import notes
+from src.database import engine
+
+model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-router = APIRouter()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@router.get('/')
-def get_notes():
-    return "return a list of note items"
-
-
-@router.post('/', status_code=status.HTTP_201_CREATED)
-def create_note():
-    return "create note item"
-
-
-@router.patch('/{noteId}')
-def update_note(noteId: str):
-    return f"update note item with id {noteId}"
-
-
-@router.get('/{noteId}')
-def get_note(noteId: str):
-    return f"get note item with id {noteId}"
-
-
-@router.delete('/{noteId}')
-def delete_note(noteId: str):
-    return f"delete note item with id {noteId}"
-
-
-app.include_router(router, tags=['Notes'], prefix='/api/notes')
+app.include_router(notes.router, tags=['Notes'], prefix='/api/notes')
 
 
 @app.get("/api/healthchecker")
 def root():
     return {"message": "Welcome to FastAPI with SQLAlchemy"}
-
